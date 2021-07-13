0618ac116ad888e63ede00434481d8e38d7119bf
Vinicius <vinireissoares@hotmail.com>
2021-07-13 18:09:19 -0300

    first commit

--- esnext/asyncAwait.js
+++ esnext/asyncAwait.js
@@ -24,7 +24,7 @@ const getTurma = letra => {
 } 
 
 // Recurso do ES8
-//Objetivo de simplificar o uso de promises...
+//Objetivo de simplificar o uso de promises.
 let obterAlunos = async () => {
     const ta = await getTurma('A')
     const tb = await getTurma('B')
