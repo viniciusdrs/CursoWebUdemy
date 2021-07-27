a47c60c464dc04610b4cd7c13c92a84aee4e713d
Vinicius <vinireissoares@hotmail.com>
2021-07-27 16:03:27 -0300

    Promises

--- esnext/promise1.js
+++
@@ -1,7 +0,0 @@
-new Promise(function(resolve) {
-    resolve(['Vinicius', 'São Paulo', 'Rockets', 'Chiefs'])
-})
-    .then(primeiroValor  => primeiroValor[0])
-    .then(primeiraLetra => primeiraLetra[0])
-    .then(letraMinuscula => letraMinuscula.toLowerCase())
-    .then(console.log)
