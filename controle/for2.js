4a9315a63163cd55fadbbc73ae218dcff4e075db
Vinicius <vinireissoares@hotmail.com>
2021-07-27 16:02:44 -0300

    For

--- controle/for2.js
+++ controle/for2.js
@@ -1,15 +1,20 @@
 const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
 
 for (let i in notas) {
-    console.log(`${i})  ${notas[i]}`)
-}
+    console.log(i, notas[i])
+} 
 
 const pessoa = {
+<<<<<<< HEAD
     nome: 'Vinicius',
     sobrenome: 'dos Reis Soares',
+=======
+    nome : 'Vinicius',
+    sobrenome : 'Soares',
+>>>>>>> parent of d8cef67 (novo)
     idade: 20
 }
 
-for (let atributo in pessoa) {
+for(let atributo in pessoa) {
     console.log(`${atributo} = ${pessoa[atributo]}`)
 }
